874fbf56dc3106221029aee0cc7cbe479bdcd501
archit-sagar-unbxd <archit.sagar@unbxd.com>
2023-05-03 12:30:55 +0530

    fix: icons scroll fix

--- src/server.js
+++ src/server.js
@@ -9,12 +9,14 @@ app.get("/upload", (req, res) => {
 	res.send({ upload: "THIS IS THE DEFAULT UPLOAD ROUTE" });
 });
 
+// boto3 for upload
+
 app.get("/upload/:id", (req, res) => {
 	const id = req.params.id;
 
 	axios
 		.get(
-			"https://libraries.unbxdapi.com/search-sdk/qa-framework/demo-unbxd700181503576558/fb853e3332f2645fac9d71dc63e09ec1.json"
+			"https://libraries.unbxdapi.com/search-sdk/qa-framework/demo-unbxd700181503576558-09ec1.json"
 		)
 		.then((response) => {
 			res.send(response.data);
